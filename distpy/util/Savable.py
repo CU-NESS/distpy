13f73a512c75c7c50119eb847a08d09ea9629b21
ktausch <keta6457@colorado.edu>
2018-04-29 01:54:45 -0600

    Removed loading functionality from Savable class, because that is what the Loadable class is for.

--- distpy/util/Savable.py
+++ distpy/util/Savable.py
@@ -33,40 +33,6 @@ class Savable(object):
             "every subclass of Savable and Savable should never be " +\
             "instantiated directly.")
     
-    @staticmethod
-    def load_from_hdf5_group(group):
-        """
-        A function which loads an instance of the current Savable subclass from
-        the given hdf5 file group. This function raises an error unless it is
-        implemented by all subclasses of Savable.
-        
-        group: hdf5 file group from which to load an instance of a Savable
-             subclass
-        """
-        raise NotImplementedError("This method should be implemented by " +\
-            "every subclass of Savable and Savable should never be " +\
-            "instantiated directly.")
-    
-    @classmethod
-    def load(cls, file_name, *args, **kwargs):
-        """
-        Loads an instance of a subclass of the Savable class. This method
-        raises an error unless load_from_hdf5_group is defined in the given
-        Savable subclass.
-        
-        file_name: the name of the file from which to load an instance of a
-                   subclass of the Savable class
-        
-        returns: an instance of the class which called this method
-        """
-        if have_h5py:
-            hdf5_file = h5py.File(file_name, 'r')
-            to_return = cls.load_from_hdf5_group(hdf5_file, *args, **kwargs)
-            hdf5_file.close()
-            return to_return
-        else:
-            raise no_h5py_error
-    
     def save(self, file_name, *args, **kwargs):
         """
         Saves this object in hdf5 file using the fill_hdf5_file group
